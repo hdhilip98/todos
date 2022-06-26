36e1f48120a196bfa44a80cb8d287216aadd5b45
hdhilip98 <dhiliph98@outlook.com>
2022-06-26 17:54:34 +0530

    fix responsive width styling

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,9 @@
   import TodoList from "./components/TodoList.svelte";
 </script>
 
-<div class="p-4 d-flex flex-column align-items-center gap-3">
-  <h2 class="display-2">todos</h2>
-  <div class="w-25 d-flex flex-column gap-3">
+<div class="p-4 app">
+  <h2 class="display-2 text-center">todos</h2>
+  <div class="d-flex flex-column gap-3">
     <TodoForm />
     <TodoControls />
     <TodoList />
